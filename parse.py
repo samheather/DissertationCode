47d81f361bfc2cb4ee436583c3f39e404fcd5f10
samheather <sam@heather.sh>
2015-01-20 13:28:14 +0000

    Adds ranking to end calculations

--- parse.py
+++ parse.py
@@ -4,8 +4,14 @@
 import lxml.etree
 import urllib
 import wikipedia_utils
+from pymongo import MongoClient
+import math
 
 import compare
+
+client = MongoClient('localhost', 27017)
+db = client.dis
+wordReferencePairs = db.wordReferencePairs
 
 def removeWikiChars(input):
 	for c in '!@#${}|':
@@ -46,6 +52,7 @@ def matchAmbiguousArgumentToProperty(argument, infobox):
 	currentMaxKey = None
 	for property in infobox:
 		similarityScore = compare.similarity(argument, str(property))
+		similarityScore = adjustSimilarityWithRanking(similarityScore, argument, property)
 		print similarityScore, ' - ', argument, ' - ', property
 		if (similarityScore > localMax):
 			localMax = similarityScore
@@ -54,3 +61,19 @@ def matchAmbiguousArgumentToProperty(argument, infobox):
 			
 	# Now simply use the key with the largest similarity to retrieve the results
 	return infobox[currentMaxKey], currentMaxKey
+	
+def adjustSimilarityWithRanking(similarity, argument, property):
+	adjustment = getQualityMultiplier(argument, property)
+	if (adjustment < 0):
+		return similarity * math.pow(0.95,abs(adjustment))
+	elif (adjustment > 0):
+		return similarity * math.pow(1.05,abs(adjustment))
+	
+def getQualityMultiplier(argument, property):
+	wordReferencePairs.find_one({
+					'givenProperty' : argument,
+					'returnedProperty' : property
+				})
+	if (wordReferencePairs == None):
+		return None
+	return wordReferencePairs['ranking']
